629504f99d830378bfd60993de328d48eab4593c
chrispapa2000 <el18017@mail.ntua.gr>
2021-12-18 15:20:44 +0200

    Reset Vehicles is basically ready

--- backend/ResetVehicles.py
+++ backend/ResetVehicles.py
@@ -1,1 +1,46 @@
-ls
+import sys
+import mariadb
+import csv
+
+def main():
+
+    try:
+        conn = mariadb.connect(
+            user="tolltrolls",
+            password="diakopeskantere",
+            host="tolltrolls.tk",
+            port=3306, #???
+            database="tolltrolls"
+        )
+
+    except mariadb.Error as e:
+        #print(f"Error connecting to MariaDB Platform: {e}")
+        #print("unhealthy")
+        sys.exit(1)
+
+    # Get Cursor
+    cur = conn.cursor()
+    statement = "SET FOREIGN_KEY_CHECKS = 0"
+    cur.execute(statement)
+    conn.commit()
+
+    statement = "TRUNCATE TABLE vehicle"
+    cur.execute(statement)
+    conn.commit()
+
+    statement = "SET FOREIGN_KEY_CHECKS = 1"
+    cur.execute(statement)
+    conn.commit()
+
+    #vehicle
+    file = open("initial_data/vehicles.csv")
+    csvreader = csv.reader(file)
+    for row in csvreader:
+        statement = "INSERT INTO vehicle (Vehicle_ref) VALUES (%s)"
+        cur.execute(statement, row)
+        conn.commit()
+    file.close()
+
+    conn.close()
+
+main()
