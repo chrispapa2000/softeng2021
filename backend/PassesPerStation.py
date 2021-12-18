f9d33ebe3f5b54b3877a9fcbd89909f589d57ea7
Ninostorm <nvamvakaris@gmail.com>
2021-12-18 19:44:14 +0200

    made Passes per station output the desired data

--- backend/PassesPerStation.py
+++ backend/PassesPerStation.py
@@ -25,8 +25,19 @@ def main():
     cur.execute("SELECT * FROM Pass WHERE stationStation_id = %s", (s_id,))
     result = cur.fetchall()
     #jsonObj = json.dumps(result)
+    #print(result)
+    print(len(result))
+    i = 1
     for entry in result:
-        print(entry)
+        str_time = entry[1].strftime("%Y")#/%m/%d %H:%M:%S")
+        PassType = "visitor"
+        if entry[3][0:2] == entry[4]:
+            PassType = "home"
+        cur.execute("SELECT Charge_amount FROM charge WHERE PassPass_id = %s", (entry[0],))
+        charge_amount = cur.fetchall()[0][0]
+        tup = (i, entry[0], str_time, entry[2], entry[4], PassType, charge_amount)
+        print(tup)
+        i += 1
     #print(jsonObj)
     """
     for line in result:
