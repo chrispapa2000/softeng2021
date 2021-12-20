752673966dac2ecc24631d1689b4fb7def60c416
chrispapa2000 <el18017@mail.ntua.gr>
2021-12-20 19:30:26 +0200

    data to csv in PassesAnalysis

--- api/PassesAnalysis.js
+++ api/PassesAnalysis.js
@@ -7,6 +7,40 @@ module.exports = function(app)
     .get(fun);
 }
 
+function ConvertObjToCSV(obj) {
+            var str = '';
+
+            var line = '';
+            for (var index in obj) {
+                if (line != '') line += ','
+
+                line += obj[index];
+            }
+
+            str += line + '\r\n';
+
+
+            return str;
+}
+
+function ConvertToCSV(objArray) {
+    var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+    var str = '';
+
+    for (var i = 0; i < array.length; i++) {
+        var line = '';
+        for (var index in array[i]) {
+            if (line != '') line += ','
+
+            line += array[i][index];
+        }
+        if (i === (array.length-1)) str += line + '"' + '\r\n';
+        else str += line + '\r\n';
+    }
+
+    return str;
+}
+
 function fun (request, response) {
     var op1 = request.params.op1_ID;
     var op2 = request.params.op2_ID;
@@ -32,7 +66,57 @@ function fun (request, response) {
         //if python script closed without errors
         //send data to browser
         response.statusCode = 200; //status ok
-        response.send(dataToSend.join(""))
+        toSend = dataToSend.join("");
+        if (false)
+        {
+          response.send(toSend)
+        }
+        else{
+          var header1 = {
+            "op1_ID": "op1_ID",
+            "op2_ID": "op2_ID",
+            "RequestTimestamp": "RequestTimestamp",
+            "PeriodFrom": "PeriodFrom",
+            "PeriodTo": "PeriodTo",
+            "NumberOfPasses": "NumberOfPasses",
+            "PassesList": "PassesList"
+          };
+
+          var csvContent = ConvertObjToCSV(header1);
+          var obj = JSON.parse(toSend);
+
+          csvContent += obj.op1_ID;
+          csvContent += ',';
+          csvContent += obj.op2_ID;
+          csvContent += ',';
+          csvContent += obj.RequestTimestamp;
+          csvContent += ',';
+          csvContent += obj.PeriodFrom;
+          csvContent += ',';
+          csvContent += obj.PeriodTo;
+          csvContent += ',';
+          csvContent += obj.NumberOfPasses;
+          csvContent += ',';
+          csvContent += '"';
+
+          var header2 = {
+            "PassIndex": "PassIndex",
+            "PassID": "PassID",
+            "StationID": "StationID",
+            "TimeStamp": "TimeStamp",
+            "VehicleID": "VehicleID",
+            "Charge": "Charge"
+          };
+
+          csvContent += ConvertObjToCSV(header2);
+
+          //csvContent += '\n';
+
+          var passesList = obj.PassesList;
+          csvContent += ConvertToCSV(passesList);
+          console.log(csvContent);
+          response.send(csvContent);
+        }
       }
       else
       {
