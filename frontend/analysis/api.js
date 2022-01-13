b1886354eba9dd0f60750ac480b919c3d07ec452
Zacharias Anastasiadis <el18161@mail.ntua.gr>
2022-01-13 11:13:22 -0500

    removed https from js files, won't work for everyone

--- frontend/analysis/api.js
+++ frontend/analysis/api.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
 		var datefrom = document.getElementById("date-from").value.replaceAll('-', '');
 		var dateto = document.getElementById("date-to").value.replaceAll('-', '');
 		//dateto.replace('-', '');
-		let apistr = "https://tolltrolls.tk:9103/interoperability/api/PassesAnalysis";
+		let apistr = "http://localhost:9103/interoperability/api/PassesAnalysis";
 		// api url
 		const api_url = apistr.concat('/', op1, '/', op2, '/', datefrom, '/', dateto);
 		// Defining async function
